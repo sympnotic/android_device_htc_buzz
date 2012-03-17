3391e3bc35ea8ff2459e7d34636fc882dc26cad9
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-03-17 18:15:39 +0100

    audio: set mRecordState in AudioInputStreamMSM72xx::set
    This fixes audio input/recording, including voice dialer and
    voice search.
    
    Thanks to Ethan Chen

--- libaudio/AudioHardware.cpp
+++ libaudio/AudioHardware.cpp
@@ -964,6 +964,7 @@ status_t AudioHardware::AudioStreamInMSM72xx::set(
 
     //mHardware->setMicMute_nosync(false);
     mState = AUDIO_INPUT_OPENED;
+    mHardware->set_mRecordState(true);
 
     if (!acoustic)
         return NO_ERROR;
