3475807e8c99af40d460fafa25f927c65749cbc6
ErwinP <erwin.pannecoucke@gmail.com>
2012-07-04 22:06:37 +0200

    Revert "CameraHAL: added Sympnotics changes"
    This reverts commit c9bd04dffe8c961aab4d4219642853eec439d0ed.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@
 # WARNING: This line must come *before* including the proprietary
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
-USE_CAMERA_STUB := false
+USE_CAMERA_STUB := true
 BUILD_LIBCAMERA := true
 
 # inherit from the proprietary version
