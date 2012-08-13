36cd9a03d70b11f378ef019fd2c8e6075f117050
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-08-13 23:52:56 +0200

    Update patch script
    Change-Id: I1114f64e55090cc1a6d88aad5e4ac71f15649181

--- patches/ApplyPatches.sh
+++ patches/ApplyPatches.sh
@@ -1,35 +1,38 @@
 #!/bin/sh
+echo "removing old patches"
+rm ../../../../bionic/*.patch
+rm ../../../../frameworks/native/*.patch
+rm ../../../../frameworks/av/*.patch
+rm ../../../../hardware/ril/*.patch
+echo
 
 echo "Copying files"
 cp android_bionic* ../../../../bionic/
 cp frameworks_native* ../../../../frameworks/native/
 cp frameworks_av* ../../../../frameworks/av/
 cp hardware_ril* ../../../../hardware/ril/
-cp hardware_qcom_display* ../../../../hardware/qcom/display/
+echo
 
 echo "cd bionic"
 cd ../../../../bionic/
 echo "apply patch"
 git am *.patch
+echo
 
 echo "cd frameworks/native"
 cd ../frameworks/native/
 echo "apply patch"
 git am *.patch
+echo
 
 echo "cd frameworks/av"
 cd ../../frameworks/av/
 echo "apply patch"
 git am *.patch
+echo
 
 echo "cd hardware/ril"
 cd ../../hardware/ril/
 echo "apply patch"
-git am *.patch
-
-echo "cd hardware/qcom/display"
-cd ../../hardware/qcom/display/
-echo "apply patch"
-git am *.patch
-
-cd ../../../
+git am *.patchrm 
+echo
