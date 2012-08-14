9cf625375a4e67d97199a3eb513c33c09865e195
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-08-14 08:33:15 +0200

    Update patch script
    Change-Id: I34c18d264499699184a5e080effbe74d50498a21

--- patches/ApplyPatches.sh
+++ patches/ApplyPatches.sh
@@ -4,12 +4,14 @@ rm ../../../../bionic/*.patch
 rm ../../../../frameworks/native/*.patch
 rm ../../../../frameworks/av/*.patch
 rm ../../../../hardware/ril/*.patch
+rm ../../../../hardware/qcom/display/*.patch
 echo
 echo "Copying files..."
 cp android_bionic* ../../../../bionic/
 cp frameworks_native* ../../../../frameworks/native/
 cp frameworks_av* ../../../../frameworks/av/
 cp hardware_ril* ../../../../hardware/ril/
+cp hardware_qcom_display* ../../../../hardware/qcom/display/
 echo
 
 echo "cd bionic"
@@ -35,4 +37,10 @@ cd ../../hardware/ril/
 echo "apply patch"
 git am *.patch
 echo
-cd ../../
+
+echo "cd hardware/qcom/display/"
+cd ../../hardware/qcom/display/
+echo "apply patch"
+git am *.patch
+echo
+cd ../../../
