340eb8b86afd690992603cda32a077d872153261
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-08-18 11:03:29 +0200

    Fix first frame on rotation animation for msm7k
    Modify rotation animation to use a screenshot instead of a black screen.
    
    Original Author: Shane Francis
    Original commit: http://review.cyanogenmod.com/#/c/18538/1
    
    Change-Id: I15619f65ffc1b8ec066360b7947c974e239472f7

--- patches/ApplyPatches.sh
+++ patches/ApplyPatches.sh
@@ -3,6 +3,7 @@ echo "removing old patches..."
 rm ../../../../bionic/*.patch
 rm ../../../../frameworks/native/*.patch
 rm ../../../../frameworks/av/*.patch
+rm ../../../../frameworks/base/*.patch
 rm ../../../../hardware/ril/*.patch
 rm ../../../../hardware/qcom/display/*.patch
 echo
@@ -10,6 +11,7 @@ echo "Copying files..."
 cp android_bionic* ../../../../bionic/
 cp frameworks_native* ../../../../frameworks/native/
 cp frameworks_av* ../../../../frameworks/av/
+cp frameworks_base* ../../../../frameworks/base/
 cp hardware_ril* ../../../../hardware/ril/
 cp hardware_qcom_display* ../../../../hardware/qcom/display/
 echo
@@ -28,6 +30,12 @@ echo
 
 echo "cd frameworks/av"
 cd ../../frameworks/av/
+echo "apply patch"
+git am *.patch
+echo
+
+echo "cd frameworks/base"
+cd ../../frameworks/base/
 echo "apply patch"
 git am *.patch
 echo
