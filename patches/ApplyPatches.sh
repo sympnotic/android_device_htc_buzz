3161a6d5ccf049e638c2706efa11dc18f681f926
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-08-27 00:31:18 +0200

    Patches: remove outdate patches
    Change-Id: Ice1bb2d514f8b98bb66424e0a4514279f86a5df3

--- patches/ApplyPatches.sh
+++ patches/ApplyPatches.sh
@@ -1,33 +1,17 @@
 #!/bin/sh
 echo "removing old patches..."
-rm ../../../../bionic/*.patch
 rm ../../../../frameworks/native/*.patch
-rm ../../../../frameworks/av/*.patch
 rm ../../../../hardware/ril/*.patch
 rm ../../../../hardware/qcom/display/*.patch
 echo
 echo "Copying files..."
-cp android_bionic* ../../../../bionic/
 cp frameworks_native* ../../../../frameworks/native/
-cp frameworks_av* ../../../../frameworks/av/
 cp hardware_ril* ../../../../hardware/ril/
 cp hardware_qcom_display* ../../../../hardware/qcom/display/
 echo
 
-echo "cd bionic"
-cd ../../../../bionic/
-echo "apply patch"
-git am *.patch
-echo
-
 echo "cd frameworks/native"
-cd ../frameworks/native/
-echo "apply patch"
-git am *.patch
-echo
-
-echo "cd frameworks/av"
-cd ../../frameworks/av/
+cd ../../../../frameworks/native/
 echo "apply patch"
 git am *.patch
 echo
