338341ae1fb8330934ff3410ab1a6758139d8912
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-09-14 23:29:05 +0200

    wifi: return to basic
    Change-Id: Ia9fc6e732b38e15818997b39ce5501273226196f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,20 +44,17 @@ TARGET_BOOTLOADER_BOARD_NAME := buzz
 TARGET_OTA_ASSERT_DEVICE := buzz
 TARGET_BOARD_INFO_FILE := device/htc/buzz/board-info.txt
 
-### Wifi related defines
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WLAN_DEVICE                := bcm4329
+# Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
-
-#WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcm4329/parameters/firmware_path"
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
+BOARD_WLAN_DEVICE                := bcm4329
+WIFI_BAND                        := 802_11_ABG
+WIFI_DRIVER_MODULE_PATH          := "/lib/modules/bcm4329.ko"
 WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcm4329.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_ARG           := "iface_name=wlan0 firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
 WIFI_DRIVER_MODULE_NAME          := "bcm4329"
-WIFI_DRIVER_MODULE_ARG           := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration iface_name=wlan"
-BOARD_WLAN_DEVICE_REV            := bcm4329
-WIFI_BAND                        := 802_11_ABG
 
 ## kernel
 TARGET_KERNEL_CONFIG   		:= buzz_defconfig
