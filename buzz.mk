eb9506bdd491b9e31daf14f2ce3f0f88c788b5a5
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-09-18 11:20:18 +0200

    buzz.mk: enable QCOM tile renderer
    disable by replacing them both by 'debug.mdpcomp.logs=0'
    
    Change-Id: Ie2dd58e492bf4e2f987bd8638c1330cab4c59038

--- buzz.mk
+++ buzz.mk
@@ -113,4 +113,6 @@ $(call inherit-product-if-exists, vendor/htc/buzz/buzz-vendor-blobs.mk)
 # Interfaces
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=15
+    wifi.supplicant_scan_interval=15 \
+    debug.mdpcomp.logs=0 \
+    debug.enabletr=true
