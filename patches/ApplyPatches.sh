33bc0cb80546e3da7e825edb00054b9f4b2cd881
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-11-14 23:29:59 +0100

    Revert "Gralloc: fix from antdking double declaration/build error"
    This reverts commit e424e049dc5f88e2789c71e1edc40bf145194d30.
    Causes a build error?

--- patches/ApplyPatches.sh
+++ patches/ApplyPatches.sh
@@ -2,12 +2,14 @@
 echo "removing old patches..."
 rm ../../../../frameworks/native/*.patch
 rm ../../../../hardware/ril/*.patch
+rm ../../../../hardware/qcom/display/*.patch
 rm ../../../../external/wpa_supplicant_6/*.patch
 
 echo
 echo "Copying files..."
 cp frameworks_native* ../../../../frameworks/native/
 cp hardware_ril* ../../../../hardware/ril/
+cp hardware_qcom_display* ../../../../hardware/qcom/display/
 cp external_wpa_supplicant_6* ../../../../external/wpa_supplicant_6/
 echo
 
@@ -23,8 +25,14 @@ echo "apply patch"
 git am *.patch
 echo
 
+echo "cd hardware/qcom/display/"
+cd ../../hardware/qcom/display/
+echo "apply patch"
+git am *.patch
+echo
+
 echo "cd external/wpa_supplicant_6/"
-cd ../../external/wpa_supplicant_6/
+cd ../../../external/wpa_supplicant_6/
 echo "apply patch"
 git am *.patch
 echo
