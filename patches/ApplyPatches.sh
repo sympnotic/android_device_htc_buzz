8d8c13b09e1ba679936dd9d68bfafdd5e645c7d8
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-12-23 22:57:55 +0100

    Update patch script
    Change-Id: Id5235ba37df7442058559ea8a3528eaf55a0ba28

--- patches/ApplyPatches.sh
+++ patches/ApplyPatches.sh
@@ -3,6 +3,7 @@ echo "removing old patches..."
 rm ../../../../frameworks/native/*.patch
 rm ../../../../hardware/ril/*.patch
 rm ../../../../hardware/qcom/display/*.patch
+rm ../../../../hardware/broadcom/wlan/*.patch
 rm ../../../../external/wpa_supplicant_6/*.patch
 
 echo
@@ -10,6 +11,7 @@ echo "Copying files..."
 cp frameworks_native* ../../../../frameworks/native/
 cp hardware_ril* ../../../../hardware/ril/
 cp hardware_qcom_display* ../../../../hardware/qcom/display/
+cp hardware_broadcom_wlan* ../../../../hardware/broadcom/wlan/
 cp external_wpa_supplicant_6* ../../../../external/wpa_supplicant_6/
 echo
 
@@ -27,6 +29,12 @@ echo
 
 echo "cd hardware/qcom/display/"
 cd ../../hardware/qcom/display/
+echo "apply patch"
+git am *.patch
+echo
+
+echo "cd hardware/broadcom/wlan/"
+cd ../../../hardware/broadcom/wlan/
 echo "apply patch"
 git am *.patch
 echo
