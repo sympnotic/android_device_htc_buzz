0a0d87e5db133ce769da654d6ed9a5fbe2ac75f9
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2012-12-29 12:29:30 +0100

    Trebuchet: port wallpaper hack
    Normally the wallpaper is drawn by the system in a separate window.
    This means that the wallpaper can often be drawn out of sync with
    the launcher. This hack gets the wallpaper bitmap and draws it
    manually (in sync with the rest of the launcher). This increases
    performance greatly.
    
    This is a port from the cm-10.1 branch
    http://review.cyanogenmod.org/#/c/27326/3

--- patches/ApplyPatches.sh
+++ patches/ApplyPatches.sh
@@ -1,1 +1,16 @@
 #!/bin/sh
+echo "removing old patches..."
+rm ../../../../packages/apps/Trebuchet/*.patch
+
+echo
+echo "Copying files..."
+cp packages_apps_Trebuchet* ../../../../packages/apps/Trebuchet/
+echo
+
+echo "cd packages/apps/Trebuchet"
+cd ../../../../packages/apps/Trebuchet/
+echo "apply patch"
+git am *.patch
+echo
+
+cd ../../../
