3ef50e19458124158b8b26344a8ea7836e304e2e
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2013-01-05 09:46:19 +0100

    revert possible wifi fixes

--- patches/ApplyPatches.sh
+++ patches/ApplyPatches.sh
@@ -3,7 +3,6 @@ echo "removing old patches..."
 rm ../../../../frameworks/native/*.patch
 rm ../../../../hardware/ril/*.patch
 rm ../../../../hardware/qcom/display/*.patch
-rm ../../../../hardware/broadcom/wlan/*.patch
 rm ../../../../external/wpa_supplicant_6/*.patch
 
 echo
@@ -11,7 +10,6 @@ echo "Copying files..."
 #cp frameworks_native* ../../../../frameworks/native/
 cp hardware_ril* ../../../../hardware/ril/
 cp hardware_qcom_display* ../../../../hardware/qcom/display/
-cp hardware_broadcom_wlan* ../../../../hardware/broadcom/wlan/
 cp external_wpa_supplicant_6* ../../../../external/wpa_supplicant_6/
 echo
 
@@ -29,12 +27,6 @@ echo
 
 echo "cd hardware/qcom/display/"
 cd ../../hardware/qcom/display/
-echo "apply patch"
-git am *.patch
-echo
-
-echo "cd hardware/broadcom/wlan/"
-cd ../../../hardware/broadcom/wlan/
 echo "apply patch"
 git am *.patch
 echo
