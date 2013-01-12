4b991cdf033683b9b9df330c930805e46219e9a2
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2013-01-12 11:12:32 +0100

    Remove this patch
    Not needed anymore since commit 9ea157cd07510fb2d1660319f4bf37d2b8ce1f87

--- patches/Create-import_includes_for_camera_intermediates.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Creating camera.buzz_intermediates/import_includes"
-mkdir -p ../../../../out/target/product/buzz/obj/SHARED_LIBRARIES/libcamera_intermediates/
-touch ../../../../out/target/product/buzz/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes
