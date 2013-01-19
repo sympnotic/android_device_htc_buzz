a74bff20fbf69ed312c03e48d0d6c1d5001c4048
Erwin Pannecoucke <erwin.pannecoucke@gmail.com>
2013-01-19 22:42:14 +0100

    Attempt to get the mobile connection to work

--- buzz.mk
+++ buzz.mk
@@ -122,3 +122,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 #Attempting to get wifi to work
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcm4329/Android.mk)
+
+#Attempting to get a mobile connection
+$(call inherit-product, build/target/product/full_base_telephony.mk)
